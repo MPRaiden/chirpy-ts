c1c0a4c7cd7938e71759889bddb87d63621a1e22
Marin Peric Radan <marin.peric@seekandhit.com>
2025-08-11 08:35:24 +0200

    feat: basic static file server config

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+const PORT = 8080
+
+app.use(express.static("public"))
+
+const server = app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`)
+})
+
+
